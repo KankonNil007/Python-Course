48a35770c75cdad17e4e5a533d42acf1b52b878d
KankonNil007 <kankonnil@gmail.com>
2025-06-06 19:47:13 +0600

    Day 5

---
+++ 005-Python-Comments-and-Print/main.py
@@ -0,0 +1,25 @@
+# Hey Kankon, Don't remove this line
+# Author : Kankon
+# Course : Python Basics Course
+
+print("I am a good boy \nThis viewer is also a good boy/girl .")
+
+# This is a Single-line Comment
+# print("Hello Kankon")
+
+print("This is a print statement")
+
+'''
+This is a multi line comment
+Thanks for asiding by
+End of multi line comment
+'''
+
+# \n is Escape Character which writes some characters that can't be typed directly
+
+print("I am a \"good boy\" ")
+
+# Some special functions of print
+
+print("Hey", 6, 7, sep="-", end="007\n")
+print("King")
