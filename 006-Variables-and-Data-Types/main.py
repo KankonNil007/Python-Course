6910f130c29e7285b444f18c8f144eda05e9e218
KankonNil007 <kankonnil@gmail.com>
2025-06-07 14:00:02 +0600

    Day 6

---
+++ 006-Variables-and-Data-Types/main.py
@@ -0,0 +1,48 @@
+# Variables and Data Types
+
+# Number Data Type
+var1 = 43
+print(var1)
+
+# String Data Type
+var2 = "Kankon"
+print(var2)
+
+# Boolean Data Type
+var3 = True
+print(var3)
+
+# Undefined/Null Data Type
+var4 = None
+print(var4)
+
+# BODMAS Rule
+var5 = 23
+print(var1 + var5)
+
+# We can check what type of variable it is
+print(type(var1))
+print("This variable is ", type(var2))
+print("This variable is ", type(var3))
+print("This variable is ", type(var4))
+
+# There are Three types of numbers :
+# Integer, Float, Complex
+
+num1 = 23
+num2 = 34.54
+num3 = complex(8, 4)
+
+print(num1, num2, num3)
+
+# List and Tuples
+# Tuples are special kind of list that can't be changed
+
+list1 = [8, 2.3, ["apple", "banana"], [2, 3]]
+tuple1 = (("parrot", "sparrow"), ("Kankon", "Nil"))
+
+print(list1, tuple1)
+
+# Mapped / Dictionary Data Type
+dict1 = {"Name": "Kankon", "Age": 20, "CanVote": True}
+print(dict1)
