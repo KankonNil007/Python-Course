9e586fa99b2e3d34c3a06ac69a40bd70d57ccb56
KankonNil007 <kankonnil@gmail.com>
2025-06-07 22:08:25 +0600

    Day 7 and 8

---
+++ 007-Python-Operators/main.py
@@ -0,0 +1,22 @@
+# Python Operators
+
+# Addition
+print(5 + 8)
+
+# Substraction
+print(15 - 8)
+
+# Multiplication
+print(15 * 8)
+
+# Division
+print(15 / 8)
+
+# Modulus - Gives the Remainder of a Division
+print(15 % 8)
+
+# It only returns the integer value of a Division
+print(15 // 8)
+
+# Exponential Operator
+print(15 ** 3)
---
+++ 008-Calculator-Project/main.py
@@ -0,0 +1,15 @@
+# Simple Calculator Project
+"""
+Actually, I thought we have to create a real calculator and so i got suprised that we haven't learnt if-else, user input function, so how we are going to complete it.
+
+Then, I look at the answer and laughed and said to myself that i have been thinking it hardly
+
+"""
+
+a = 34
+b = 12
+
+print("The calculation of", a,"+",b,"is",a+b)
+print("The calculation of", a,"-",b,"is",a-b)
+print("The calculation of", a,"*",b,"is",a*b)
+print("The calculation of", a,"/",b,"is",a/b)
