115c3d415c2b73efdbd3a626c5a7bc1116ee887d
KankonNil007 <kankonnil@gmail.com>
2025-06-10 12:22:29 +0600

    Day 11

---
+++ 011-Strings-in-Python/main.py
@@ -0,0 +1,38 @@
+# Strings In Python
+
+name = "Kankon"
+friend = "Sagor"
+anotherFriend = "Tribindo"
+
+print("Hello, " + name)
+
+# Double / Single Quotes as Strings
+
+apple = 'He said, "I will be there in the market."'
+print(apple)
+
+# Multi Line Strings : Use Double Quotes 3 times
+
+conv = """
+Hi Kankon,
+I am Good. How about you?
+What are you up to this week?
+"""
+
+print(conv)
+
+# Indexing a String
+
+name2 = "Kankon"
+print(name2[0])
+# print(name2[1])
+# print(name2[2])
+# print(name2[3])
+# print(name2[4])
+# print(name2[5])
+
+# Looping Through a String
+
+print("Let's use a loop \n")
+for character in name2:
+    print(character)
