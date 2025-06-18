5e4816e1e46ce63d122db8155591f011c1460e8b
KankonNil007 <kankonnil@gmail.com>
2025-06-18 10:48:25 +0600

    Day 29

---
+++ 029-DocStrings-in-Python/main.py
@@ -0,0 +1,11 @@
+# Doc-Strings in Python
+
+def cube(n):
+    ''' The function takes the number n and returns the value of its Cube''' # Doc-Strings are written just below of a function, class, method or a module or else it will not work.
+    print(n**3)
+cube(5)
+print(cube.__doc__)
+
+# PEP-8 : Python Enhancement Program
+
+import this # This prints out "The Zen of Python"
