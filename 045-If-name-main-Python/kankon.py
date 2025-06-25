2e216565fdfe6bfeb449e041a3f3e5036aeef682
KankonNil007 <kankonnil@gmail.com>
2025-06-25 21:39:46 +0600

    Day 45

---
+++ 045-If-name-main-Python/kankon.py
@@ -0,0 +1,4 @@
+def welcome():
+    print("Welcome to another world!")
+if __name__ == "__main__": # if this function is not enabled, you can run the code inside a file without even a calling a function or class inside that file. This can be caused by Malware
+    welcome()
---
+++ 045-If-name-main-Python/main.py
@@ -0,0 +1,5 @@
+# if __name__ == __main__ in Python
+
+import kankon
+
+kankon.welcome() # It prints two times . so we have to solve it
