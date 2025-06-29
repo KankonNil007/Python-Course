9d729fd3eea755b3b5e87eec92d769cb6746c13b
KankonNil007 <kankonnil@gmail.com>
2025-06-29 11:19:18 +0600

    Day 56

---
+++ 056-Introduction-to-OOPs/main.py
@@ -0,0 +1,12 @@
+# Introduction to OOPs in Python
+
+# OOP stands for Object-Oriented Programming.
+
+# Classes and objects are the two core concepts in object-oriented programming.
+
+# A class defines what an object should look like, and an object is created based on that class.
+
+
+# Class	    Objects
+# Fruit	    Apple, Banana, Mango
+# Car	    Volvo, Audi, Toyota
