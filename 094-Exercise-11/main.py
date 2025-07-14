d6fbd99cc57d894d0bfc0748695a3005908c6352
KankonNil007 <kankonnil@gmail.com>
2025-07-14 19:21:18 +0600

    Day 94

---
+++ 094-Exercise-11/main.py
@@ -0,0 +1,16 @@
+# Exercise 11 - Drink Water Reminder
+
+from plyer import notification
+import time
+
+def notify():
+    notification.notify(
+        title='Drink Water Reminder',
+        message="It's time for drinking another glass of water",
+        app_name='Reminder from Python',
+        timeout=10
+    )
+
+while(True):
+    notify()
+    time.sleep(3600)
